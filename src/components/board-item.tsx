0a0aaf56e5f4e193e7e9a5a46c202524c3e55929
wakiryounus <wakiryounus@gmail.com>
2024-12-27 14:44:07 +0530

    Added Responsive

--- src/components/board-item.tsx
+++ src/components/board-item.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Character } from "../utils/character-util";
-import { makeStyles } from "@mui/styles";
 import ReactCardFlip from "react-card-flip";
-import clsx from "clsx";
 import useCardSelection from "../hooks/use-card-selection";
+import { styled } from "@mui/material";
 
 export type BoardItemProps = {
   item: Character;
@@ -12,8 +11,12 @@ export type BoardItemProps = {
   setBoardItems: React.Dispatch<React.SetStateAction<Character[]>>;
 };
 
-const useStyles = makeStyles({
-  boardItem: {
+interface BoardItemContainerProps {
+  solved: boolean;
+}
+
+const BoardItemContainer = styled("div")<BoardItemContainerProps>(
+  ({ solved }) => ({
     height: "100%",
     cursor: "pointer",
     "& img": {
@@ -30,18 +33,19 @@ const useStyles = makeStyles({
       justifyContent: "center",
       alignItems: "center",
     },
-  },
-  emptyCard: {
-    background: "#fff",
-    borderRadius: "5px",
-    width: "100%",
-    height: "100%",
-  },
-  animateOnSolve: {
-    transition: "all 1.5s ease-in-out",
-    transform: "scale(0)",
-    opacity: 0,
-  },
+    "& .react-card-flip-container": {
+      transition: "all 1.5s ease-in-out",
+      transform: !solved ? "scale(1)" : "scale(0)",
+      opacity: !solved ? 1 : 0,
+    },
+  }),
+);
+
+const EmptyCard = styled("div")({
+  background: "#fff",
+  borderRadius: "5px",
+  width: "100%",
+  height: "100%",
 });
 
 const BoardItem: React.FC<BoardItemProps> = ({
@@ -50,7 +54,6 @@ const BoardItem: React.FC<BoardItemProps> = ({
   setBoardItems,
   selectedCard,
 }) => {
-  const classes = useStyles();
   const { flipped, onCardSelect } = useCardSelection({
     setSelectedCard,
     setBoardItems,
@@ -58,18 +61,15 @@ const BoardItem: React.FC<BoardItemProps> = ({
   });
 
   return (
-    <div className={classes.boardItem}>
+    <BoardItemContainer solved={item.solved}>
       <ReactCardFlip
-        containerClassName={clsx({ [classes.animateOnSolve]: item.solved })}
+        containerClassName="react-card-flip-container"
         isFlipped={flipped}
       >
-        <div
-          className={classes.emptyCard}
-          onClick={() => onCardSelect(item)}
-        ></div>
+        <EmptyCard onClick={() => onCardSelect(item)}></EmptyCard>
         <img src={item.image} alt={item.name} />
       </ReactCardFlip>
-    </div>
+    </BoardItemContainer>
   );
 };
 
--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,39 +1,48 @@
 import React, { useState } from "react";
 import { Character, getAllCharacters } from "../utils/character-util";
-import { makeStyles } from "@mui/styles";
 import BoardItem from "./board-item";
 import GameOver from "./game-over";
+import { styled } from "@mui/material";
 
-const useStyles = makeStyles({
-  root: {
-    width: "35%",
-    paddingBottom: "35%",
-    position: "relative",
+const Root = styled("div")(({ theme }) => ({
+  width: "35%",
+  paddingBottom: "35%",
+  position: "relative",
+  [theme.breakpoints.down("md")]: {
+    width: "55%",
+    paddingBottom: "55%",
   },
-  container: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    left: 0,
-    bottom: 0,
-    display: "grid",
-    gridTemplateColumns: "repeat(6, 1fr)",
-    gridTemplateRows: "repeat(6, 1fr)",
-    gap: "5px",
+  [theme.breakpoints.down("sm")]: {
+    width: "85%",
+    paddingBottom: "85%",
   },
-});
+}));
+
+const Container = styled("div")(({ theme }) => ({
+  position: "absolute",
+  top: 0,
+  right: 0,
+  left: 0,
+  bottom: 0,
+  display: "grid",
+  gridTemplateColumns: "repeat(6, 1fr)",
+  gridTemplateRows: "repeat(6, 1fr)",
+  gap: "5px",
+  [theme.breakpoints.down("sm")]: {
+    gap: "3px",
+  },
+}));
 
 const Board = () => {
-  const classes = useStyles();
   const [selectedCard, setSelectedCard] = useState<Character[]>([]);
   const [boardItems, setBoardItems] = useState<Character[]>(getAllCharacters);
 
   const gameOver = boardItems.every((item) => item.solved);
 
   return (
-    <div className={classes.root}>
+    <Root>
       {!gameOver ? (
-        <div className={classes.container}>
+        <Container>
           {boardItems.map((item, index) => (
             <BoardItem
               item={item}
@@ -43,11 +52,11 @@ const Board = () => {
               setBoardItems={setBoardItems}
             />
           ))}
-        </div>
+        </Container>
       ) : (
         <GameOver />
       )}
-    </div>
+    </Root>
   );
 };
 
